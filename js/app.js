8b94b9cfc4953acc8e8e1e80d852c6f39bcf7237
Hugo Moragues <hugo.moragues@gmail.com>
2018-08-04 18:28:18 +0200

    minor

---
+++ js/app.js
@@ -0,0 +1,18 @@
+(function() {
+	'use strict';
+
+	window.addEventListener("deviceorientation", function(event) {
+
+		var values = [
+			Math.round(event.alpha),
+			Math.round(event.beta),
+			Math.round(event.gamma)
+			];
+
+		document.querySelectorAll(".output-result-value")
+			.forEach(function(el, index) {
+				el.innerHTML = values[index];
+			});
+		}, true);
+
+})();
